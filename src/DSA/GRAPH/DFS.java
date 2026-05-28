d9a9ae156415f0a427037554aa03a4c460027e3b
Rudra Verma <rudraverma781@gmail.com>
2026-05-28 18:03:52 +0530

    added DFS  class

---
+++ src/DSA/GRAPH/DFS.java
@@ -0,0 +1,26 @@
+package DSA.GRAPH;
+
+import java.util.ArrayList;
+
+public class DFS {
+    public static void  Travel(ArrayList<ArrayList<Integer>> adj , int vis[], ArrayList<Integer> ans, int elem){
+
+        ans.add(elem);
+        vis[elem] = 1;
+        for(int x : adj.get(elem)){
+            if(vis[x]==0){
+                Travel(adj,vis,ans,x);
+
+            }
+        }
+
+    }
+    public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        int[] vis = new int[adj.size()];
+
+        Travel(adj,vis,ans, 0);
+        return ans ;
+    }
+}
