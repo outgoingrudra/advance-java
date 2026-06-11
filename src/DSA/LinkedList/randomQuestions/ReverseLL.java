7d4d02ac7e7fc6b01ac83ad17ee617a0ce91593b
Rudra Verma <rudraverma781@gmail.com>
2026-06-11 08:15:28 +0530

    practised   LL

---
+++ src/DSA/LinkedList/randomQuestions/ReverseLL.java
@@ -0,0 +1,17 @@
+package DSA.LinkedList.randomQuestions;
+
+public class ReverseLL {
+    public ListNode reverseList(ListNode head) {
+        ListNode temp = head ;
+        ListNode prev = null ;
+        ListNode front =  null ;
+
+        while(temp != null){
+            front = temp.next ;
+            temp.next = prev ;
+            prev = temp ;
+            temp = front ;
+        }
+        return prev ;
+    }
+}
