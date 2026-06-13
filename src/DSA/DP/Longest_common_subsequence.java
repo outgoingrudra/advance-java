2cf5bb0cc74563a29c9e77f07340d6953268de61
Rudra Verma <rudraverma781@gmail.com>
2026-06-13 20:29:25 +0530

    practised   DP

--- src/DSA/DP/Longest_common_subsequence.java
+++ src/DSA/DP/Longest_common_subsequence.java
@@ -13,7 +13,7 @@ public class Longest_common_subsequence {
         }
         return memory[n][m];
     }
-    public int longestCommonSubsequence(String text1, String text2) {
+    public int longestCommonSubsequenceUsingMemory(String text1, String text2) {
         int n = text1.length();
         int m = text2.length();
 
@@ -24,6 +24,31 @@ public class Longest_common_subsequence {
             }
         }
         return count(text1,text2,n,m,memory);
+
+    }
+
+    public int longestCommonSubsequenceUsingTopDown(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+
+        int memory[][] = new int[n+1][m+1];
+        for(int i =0;i< n+1;i++){
+            for(int j =0;j< m+1;j++){
+                if(i==0 ||j==0)   memory[i][j] = 0;
+            }
+        }
+
+        for(int i =1;i< n+1;i++){
+            for(int j =1;j< m+1;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    memory[i][j] = 1+ memory[i-1][j-1];
+                }
+                else{
+                    memory[i][j] = Math.max(memory[i-1][j],memory[i][j-1]);
+                }
+            }
+        }
+        return memory[n][m];
 
     }
 }
