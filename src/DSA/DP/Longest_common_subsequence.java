3365775c7612849144b8dd2ae8a764d834ba645d
Rudra Verma <rudraverma781@gmail.com>
2026-06-14 13:10:02 +0530

    practised   DP

--- src/DSA/DP/Longest_common_subsequence.java
+++ src/DSA/DP/Longest_common_subsequence.java
@@ -51,4 +51,50 @@ public class Longest_common_subsequence {
         return memory[n][m];
 
     }
+
+    public static  String longestCommonSubsequenceUsingTopDownPrint(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+
+        int memory[][] = new int[n+1][m+1];
+        for(int i =0;i< n+1;i++){
+            for(int j =0;j< m+1;j++){
+                if(i==0 ||j==0)   memory[i][j] = 0;
+            }
+        }
+
+        for(int i =1;i< n+1;i++){
+            for(int j =1;j< m+1;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    memory[i][j] = 1+ memory[i-1][j-1];
+                }
+                else{
+                    memory[i][j] = Math.max(memory[i-1][j],memory[i][j-1]);
+                }
+            }
+        }
+
+        int i = n, j = m;
+        String s="" ;
+        while(i>0 && j>0){
+            if(text1.charAt(i-1)==text2.charAt(j-1)){
+                s = text1.charAt(i-1) + s;
+                i--;
+                j--;
+            }
+            else{
+                if(memory[i][j-1]>memory[i-1][j]) j--;
+                else i--;
+            }
+
+        }
+
+        return s;
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestCommonSubsequenceUsingTopDownPrint("acbcf","abcdaf"));
+    }
 }
