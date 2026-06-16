00a58d894efa64d0485535f5f1ceb69fb69ad974
Rudra Verma <rudraverma781@gmail.com>
2026-06-16 12:18:36 +0530

    practised   DP

---
+++ src/DSA/DP/Sequence_Pattern_Matching.java
@@ -0,0 +1,32 @@
+package DSA.DP;
+
+public class Sequence_Pattern_Matching {
+    public int lcs(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+
+        int memory[][] = new int[n+1][m+1];
+        for(int i =0;i< n+1;i++){
+            for(int j =0;j< m+1;j++){
+                if(i==0 ||j==0)   memory[i][j] = 0;
+            }
+        }
+
+        for(int i =1;i< n+1;i++){
+            for(int j =1;j< m+1;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    memory[i][j] = 1+ memory[i-1][j-1];
+                }
+                else{
+                    memory[i][j] = Math.max(memory[i-1][j],memory[i][j-1]);
+                }
+            }
+        }
+        return memory[n][m];
+
+    }
+    public boolean isSubSeq(String s1, String s2) {
+        // code here
+        return lcs(s1,s2)==s1.length();
+    }
+}
