a7905470478fcb00a2088b82eac106b9949c71c7
Rudra Verma <rudraverma781@gmail.com>
2026-06-21 10:23:10 +0530

    practised   Graph

--- src/DSA/GRAPH/DetectCycle_Directed_Graph.java
+++ src/DSA/GRAPH/DetectCycle_Directed_Graph.java
@@ -1,7 +1,9 @@
 package DSA.GRAPH;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class DetectCycle_Directed_Graph {
     public boolean DFS(int node , List<List<Integer>> adj , int vis[] ,int pathVis[]){
@@ -35,5 +37,37 @@ public class DetectCycle_Directed_Graph {
         }
         return false ;
 
+    }
+
+    public boolean isCyclic_Using_TopoSort(int V, int[][] edges) {
+        // code here
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i =0;i<V;i++) adj.add(new ArrayList<>());
+        for(int it[] : edges) adj.get(it[0]).add(it[1]);
+        int indegree[] = new int[V];
+        for(int i =0;i<V;i++){
+            for(int it : adj.get(i)){
+                indegree[it]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for(int i = 0;i< V;i++){
+            if(indegree[i]==0) q.offer(i);
+        }
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+            ans.add(node);
+            if(ans.size()==V) return false ;
+            for(int it : adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0) q.add(it);
+            }
+
+        }
+        return true   ;
     }
 }
