04555715818930ee248e049b027a0776043682fc
Rudra Verma <rudraverma781@gmail.com>
2026-07-10 18:05:50 +0530

    practised  Greedy

---
+++ src/DSA/greedy/lemonade.java
@@ -0,0 +1,32 @@
+package DSA.greedy;
+
+public class lemonade {
+    public boolean lemonadeChange(int[] bills) {
+        int f = 0;
+        int t = 0;
+        int t2 = 0;
+        for(int  x : bills ){
+            if(x==5) f++;
+            else{
+                if(f==0) return false ;
+                if(x==10){
+                    t++;
+                    f-- ;
+                }
+                else{
+                    if(t>0){
+                        t--;
+                        f--;
+                    }
+                    else {
+                        if(f>=3){
+                            f = f- 3;
+                        }
+                        else return false ;
+                    }
+                }
+            }
+        }
+        return true ;
+    }
+}
